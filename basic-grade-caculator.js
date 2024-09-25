03c2207a3679f152fd7d53c698beb00e1a1614bd
Jake Maloney <115516507+j4ke-m4loney@users.noreply.github.com>
2024-09-25 13:27:48 +1000

    basic-grade-calculator

---
+++ basic-grade-caculator.js
@@ -0,0 +1,20 @@
+function getGrade(score) {
+  if (score === 100) {
+    return "A++";
+  } else if (score >= 90 && score <= 99) {
+    return "A";
+  } else if (score >= 80 && score <= 89) {
+    return "B";
+  } else if (score >= 70 && score <= 79) {
+    return "C";
+  } else if (score >= 60 && score <= 69) {
+    return "D";
+  } else if (score >= 6 && score <= 59) {
+    return "F";
+  } else
+    return "Bad Luck"
+}
+
+console.log(getGrade(96));
+console.log(getGrade(82));
+console.log(getGrade(56));
