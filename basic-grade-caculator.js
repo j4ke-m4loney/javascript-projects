bbb8fa044dd675564e80c5463d2bad5116c7a228
Jake Maloney <115516507+j4ke-m4loney@users.noreply.github.com>
2024-09-29 20:15:14 +1000

    message-students-results

--- basic-grade-caculator.js
+++ basic-grade-caculator.js
@@ -32,3 +32,15 @@ function hasPassingGrade(score) {
 console.log(hasPassingGrade(100));
 console.log(hasPassingGrade(53));
 console.log(hasPassingGrade(87));
+
+// Message students the results
+
+function studentMsg(totalScores, studentScore) {
+  if (hasPassingGrade(studentScore) == true) {
+    return ("Class average: " + getAverage(totalScores) + ". Your grade: " + getGrade(studentScore) + ". You passed the course.");
+  } else {
+    return ("Class average: " + getAverage(totalScores) + ". Your grade: " + getGrade(studentScore) + ". You failed the course.");
+  }
+}
+
+console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
