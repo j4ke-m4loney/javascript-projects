2393f9758bfde9ade41d744e874db9f3d201965f
Jake Maloney <115516507+j4ke-m4loney@users.noreply.github.com>
2024-10-11 12:05:27 +1100

    added-isInvalidInput/function

---
+++ calorie-counter/scripts-calorie-counter.js
@@ -0,0 +1,21 @@
+const calorieCounter = document.getElementById('calorie-counter');
+const budgetNumberInput = document.getElementById('budget');
+const entryDropdown = document.getElementById('entry-dropdown');
+const addEntryButton = document.getElementById('add-entry');
+const clearButton = document.getElementById('clear');
+const output = document.getElementById('output');
+let isError = false;
+
+
+// To clear all characters from inputs
+function cleanInputString(str) {
+  const regex = /[+-\s]/g;
+  return str.replace(regex, '');
+}
+
+// To remove invalid inputs regex 
+function isInvalidInput(str) {
+  const regex = /\d+e\d+/i;
+  return str.match(regex);
+}
+// result is null from test: console.log(isInvalidInput("10"));
