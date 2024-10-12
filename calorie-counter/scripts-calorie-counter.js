882c59786e21d7f72ed9c97a7575a3ad387728a5
Jake Maloney <115516507+j4ke-m4loney@users.noreply.github.com>
2024-10-12 12:33:30 +1100

    new-htmlForContent-targetInputContainer

--- calorie-counter/scripts-calorie-counter.js
+++ calorie-counter/scripts-calorie-counter.js
@@ -22,6 +22,17 @@ function isInvalidInput(str) {
 
 // Accepting entries from dropdown
 function addEntry() {
-  const targetId = '#' + entryDropdown.value;
-  const targetInputContainer = document.querySelector(targetId + ' .input-container');
+  const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+  const HTMLString = `
+  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+  <input
+    type="number"
+    min="0"
+    id="${entryDropdown.value}-${entryNumber}-calories"
+    placeholder="Calories"
+  />`;
+
 }
