c5644c23ba0086116bc60f927337b30d2d592cb9
Jake Maloney <115516507+j4ke-m4loney@users.noreply.github.com>
2024-10-14 21:51:58 +1100

    updated-comments

--- rock-paper-scissors-game/rock-scripts.js
+++ rock-paper-scissors-game/rock-scripts.js
@@ -9,7 +9,7 @@ function getRandomComputerResult() {
   - Multiplying by options.length ensures the random number ranges between 0 and the length of the options array
   - Math.floor() rounds the result down to the nearest whole number, giving a valid array index (0, 1, or 2)
   - randomIndex now holds a random index between 0 and the length of the options array minus 1
-  - return options[randomIndex]; // Return a random choice from the array 
+  - return options[randomIndex]; Returns a random choice from the array 
   */
 
 }
