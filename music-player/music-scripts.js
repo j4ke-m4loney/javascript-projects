2c2d3cd839b4b56db5bbbe5c61e2d8297d1f99de
Jake Maloney <115516507+j4ke-m4loney@users.noreply.github.com>
2024-10-24 21:54:16 +1100

    updated-playPreviousSong

--- music-player/music-scripts.js
+++ music-player/music-scripts.js
@@ -121,7 +121,13 @@ const playNextSong = () => {
 };
 
 const playPreviousSong = () => {
+  if (userData?.currentSong === null) return;
+  else {
+    const currentSongIndex = getCurrentSongIndex();
+    const previousSong = userData?.songs[currentSongIndex - 1];
 
+    playSong(previousSong.id);
+  }
 };
 
 const renderSongs = (array) => {
