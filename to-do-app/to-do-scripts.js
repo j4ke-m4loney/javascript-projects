b53d49b79bb26ac147a49203038a3011c5d846bf
Jake Maloney <115516507+j4ke-m4loney@users.noreply.github.com>
2024-11-02 16:16:38 +1100

    created-edit-task-function
    This function allows users to edit previous tasks they added

--- to-do-app/to-do-scripts.js
+++ to-do-app/to-do-scripts.js
@@ -26,6 +26,7 @@ const addOrUpdateTask = () => {
     taskData.unshift(taskObj);
   }
 
+
   updateTaskContainer()
   reset()
 };
@@ -41,7 +42,7 @@ const updateTaskContainer = () => {
           <p><strong>Date:</strong> ${date}</p>
           <p><strong>Description:</strong> ${description}</p>
           <button onclick="editTask(this)" type="button" class="btn">Edit</button>
-          <button onclick="deleteTask(this)" type="button" class="btn">Delete</button>
+          <button onclick="deleteTask(this)" type="button" class="btn">Delete</button> 
         </div>
       `
     }
@@ -59,7 +60,19 @@ const deleteTask = (buttonEl) => {
 }
 
 const editTask = (buttonEl) => {
+  const dataArrIndex = taskData.findIndex(
+    (item) => item.id === buttonEl.parentElement.id
+  );
 
+  currentTask = taskData[dataArrIndex];
+
+  titleInput.value = currentTask.title;
+  dateInput.value = currentTask.date;
+  descriptionInput.value = currentTask.description;
+
+  addOrUpdateTaskBtn.innerText = "Update Task";
+
+  taskForm.classList.toggle("hidden");
 }
 
 const reset = () => {
