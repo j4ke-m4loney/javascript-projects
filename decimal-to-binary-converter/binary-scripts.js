eb86918e1707b7a9a832c525368f8e7a5cab5637
Jake Maloney <115516507+j4ke-m4loney@users.noreply.github.com>
2024-11-04 19:34:41 +1100

    working on decimalToBinary

--- decimal-to-binary-converter/binary-scripts.js
+++ decimal-to-binary-converter/binary-scripts.js
@@ -2,16 +2,40 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
+const decimalToBinary = (input) => {
+  const inputs = [];
+  const quotients = [];
+  const remainders = [];
+
+  while (input > 0) {
+    const quotient = Math.floor(input / 2);
+    const remainder = input % 2;
+
+    inputs.push(input);
+    quotients.push(quotient);
+    remainders.push(remainder);
+    input = quotient;
+  }
+
+  console.log("Inputs: ", inputs);
+  console.log("Quotients: ", quotients);
+  console.log("Remainders: ", remainders);
+
+
+};
+
 const checkUserInput = () => {
   if (
     !numberInput.value ||
     isNaN(parseInt(numberInput.value)) ||
     parseInt(numberInput.value) < 0
   ) {
-
+    alert("Please provide a decimal number greater than or equal to 0");
+    return;
   }
 
-  console.log(numberInput.value);
+  decimalToBinary(parseInt(numberInput.value));
+  numberInput.value = "";
 };
 
 convertBtn.addEventListener("click", checkUserInput);
