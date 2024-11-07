ea3c4af047db447ecff100133a8f508de04a8bdd
jakemaloney <jakemal85@gmail.com>
2024-11-07 15:42:41 +1100

    updating showAnimation

--- decimal-to-binary-converter/binary-scripts.js
+++ decimal-to-binary-converter/binary-scripts.js
@@ -3,26 +3,33 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
-  if (input === 0) {
-    return "0";
-  } else if (input === 1) {
-    return "1";
+  if (input === 0 || input === 1) {
+    return String(input);
   } else {
     return decimalToBinary(Math.floor(input / 2)) + (input % 2);
   }
 };
 
+const showAnimation = () => {
+  console.log("free");
+  console.log("Code");
+  console.log("Camp");
+};
+
 const checkUserInput = () => {
-  if (
-    !numberInput.value ||
-    isNaN(parseInt(numberInput.value)) ||
-    parseInt(numberInput.value) < 0
-  ) {
+  const inputInt = parseInt(numberInput.value);
+
+  if (!numberInput.value || isNaN(inputInt) || inputInt < 0) {
     alert("Please provide a decimal number greater than or equal to 0");
     return;
   }
 
-  result.textContent = decimalToBinary(parseInt(numberInput.value));
+  if (inputInt === 5) {
+    showAnimation();
+    return;
+  }
+
+  result.textContent = decimalToBinary(inputInt);
   numberInput.value = "";
 };
 
