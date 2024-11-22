b99a9ca2e288a13c3b299d58b28f6d145e216eee
jakemaloney <jakemal85@gmail.com>
2024-11-22 16:16:35 +1100

    updated counts[el]

--- statistics-calculator/scripts-calc.js
+++ statistics-calculator/scripts-calc.js
@@ -1,16 +1,25 @@
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
 
 const getMedian = (array) => {
-  const sorted = array.toSorted((a, b) => a - b);
+  const sorted = array.sort((a, b) => a - b);
   const median =
-    sorted.length % 2 === 0
-      ? getMean([sorted[sorted.length / 2], sorted[sorted.length / 2 - 1]])
-      : sorted[Math.floor(sorted.length / 2)];
+    array.length % 2 === 0
+      ? getMean([sorted[array.length / 2], sorted[array.length / 2 - 1]])
+      : sorted[Math.floor(array.length / 2)];
   return median;
 }
 
-const getMode = (array) => { };
-
+const getMode = (array) => {
+  const counts = {};
+  array.forEach(el => {
+    if (counts[el]) {
+      counts[el] += 1;
+    } else {
+      counts[el] = 1;
+    }
+  });
+  return counts;
+}
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
@@ -19,6 +28,7 @@ const calculate = () => {
 
   const mean = getMean(numbers);
   const median = getMedian(numbers);
+  console.log(getMode(numbers));
 
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
