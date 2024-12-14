f405ed2e4c6f70eafd6a979c1c16f6c539180ca8
jakemaloney <jakemal85@gmail.com>
2024-12-14 22:47:35 +1100

    Updated collision logic
    checks if the sum of the player's y position, height, and y velocity is greater than or equal to the platform's y position.

--- platformer-game/platformer-app.js
+++ platformer-game/platformer-app.js
@@ -122,7 +122,10 @@ const animate = () => {
 
 
   platforms.forEach((platform) => {
-    const collisionDetectionRules = [player.position.y + player.height <= platform.position.y];
+    const collisionDetectionRules = [
+      player.position.y + player.height <= platform.position.y,
+      player.position.y + player.height + player.velocity.y >= platform.position.y,
+    ];
   });
 
 }
