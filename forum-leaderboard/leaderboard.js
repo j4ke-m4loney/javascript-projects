ff6eda16bde11b2e2e9158f3252afac2cecf2f5f
jakemaloney <jakemal85@gmail.com>
2025-01-05 15:17:26 +1100

    created timeAgo function for Activity column

--- forum-leaderboard/leaderboard.js
+++ forum-leaderboard/leaderboard.js
@@ -8,6 +8,25 @@ const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 
 const postsContainer = document.getElementById("posts-container");
 
+// Display data in Activity column 
+const timeAgo = (time) => {
+  const currentTime = new Date(); // the current time and date
+  const lastPost = new Date(time); // the date of the last activity on a topic,
+  const minutes = Math.floor((currentTime - lastPost) / 60000);
+  const hours = Math.floor((currentTime - lastPost) / 3600000);
+  const days = Math.floor((currentTime - lastPost) / 86400000);
+  // If the amount of minutes that have passed is less than 60
+  if (minutes < 60) {
+    return `${minutes}m ago`;
+  }
+  // If the amount of hours that have passed is less than 24
+  if (hours < 24) {
+    return `${hours}h ago`;
+  } else {
+    return `${days}d ago`;
+  }
+};
+
 // To populate the forum leaderboard with data, you need to request the data from an API. 
 // It's known as an asynchronous operation, which means that tasks execute independently of the main program flow.
 // Using the async keyword to create an asynchronous function, which returns a promise.
