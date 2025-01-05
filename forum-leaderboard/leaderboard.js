f2d6453806204fa045647368d95c7b60caba1c4c
jakemaloney <jakemal85@gmail.com>
2025-01-05 13:33:42 +1100

    setup debugging with npm
    npm install jsdom

---
+++ forum-leaderboard/leaderboard.js
@@ -0,0 +1,22 @@
+const { JSDOM } = require("jsdom");
+const { document } = new JSDOM('<!doctype html><html><body><div id="posts-container"></div></body></html>').window;
+
+const forumLatest = "https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json";
+const forumTopicUrl = "https://forum.freecodecamp.org/t/";
+const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
+const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
+
+const postsContainer = document.getElementById("posts-container");
+
+// To populate the forum leaderboard with data, you need to request the data from an API. 
+// It's known as an asynchronous operation, which means that tasks execute independently of the main program flow.
+// Using the async keyword to create an asynchronous function, which returns a promise.
+const fetchData = async () => {
+  try {
+    const res = await fetch(forumLatest);
+    const data = await res.json();
+    console.log(data);
+  } catch (err) {
+
+  }
+};
