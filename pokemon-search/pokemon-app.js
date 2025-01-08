1f56ab3909447013b1d0e972d5aef58eccc0148f
jakemaloney <jakemal85@gmail.com>
2025-01-09 10:02:29 +1100

    fixed some syntax errors

--- pokemon-search/pokemon-app.js
+++ pokemon-search/pokemon-app.js
@@ -21,31 +21,31 @@ const getPokemon = async () => {
     );
     const data = await response.json();
 
-    // Set Pokemon info
+    // Set Pokémon info
     pokemonName.textContent = `${data.name.toUpperCase()}`;
     pokemonID.textContent = `#${data.id}`;
     weight.textContent = `Weight: ${data.weight}`;
     height.textContent = `Height: ${data.height}`;
     spriteContainer.innerHTML = `
-    <img id="sprite" src="${data.sprites.front_default}" alt="${data.name} front default sprite">`;
+      <img id="sprite" src="${data.sprites.front_default}" alt="${data.name} front default sprite">
+    `;
 
     // Set stats
     hp.textContent = data.stats[0].base_stat;
     attack.textContent = data.stats[1].base_stat;
     defense.textContent = data.stats[2].base_stat;
     specialAttack.textContent = data.stats[3].base_stat;
-    specialDefense.textContent = data.stat[4].base_stat;
+    specialDefense.textContent = data.stats[4].base_stat;
     speed.textContent = data.stats[5].base_stat;
 
     // Set types
-
     types.innerHTML = data.types
       .map(obj => `<span class="type ${obj.type.name}">${obj.type.name}</span>`)
       .join('');
   } catch (err) {
     resetDisplay();
-    alert('Pokemon not found');
-    console.log(`Pokemon not found: ${err}`);
+    alert('Pokémon not found');
+    console.log(`Pokémon not found: ${err}`);
   }
 };
 
@@ -54,7 +54,7 @@ const resetDisplay = () => {
   if (sprite) sprite.remove();
 
   // reset stats
-  pokemonName.textContent = ``;
+  pokemonName.textContent = '';
   pokemonID.textContent = '';
   types.innerHTML = '';
   height.textContent = '';
@@ -70,4 +70,4 @@ const resetDisplay = () => {
 searchForm.addEventListener('submit', e => {
   e.preventDefault();
   getPokemon();
-})
+});
