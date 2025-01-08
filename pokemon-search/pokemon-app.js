cca869374501a2d678dc5f105ced3f683cfb58a7
jakemaloney <jakemal85@gmail.com>
2025-01-09 09:43:37 +1100

    added css and variables in js

---
+++ pokemon-search/pokemon-app.js
@@ -0,0 +1,29 @@
+const pokemonID = document.getElementById('pokemon-id');
+const pokemonName = document.getElementById('pokemon-name');
+const spriteContainer = document.getElementById('sprite-container');
+const types = document.getElementById('types');
+const height = document.getElementById('height');
+const weight = document.getElementById('weight');
+const hp = document.getElementById('hp');
+const attack = document.getElementById('attack');
+const defense = document.getElementById('defense');
+const specialAttack = document.getElementById('special-attack');
+const specialDefense = document.getElementById('special-defense');
+const speed = document.getElementById('speed');
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+
+const getPokemon = async () => {
+  try {
+    const pokemonNameOrId = searchInput.value.toLowerCase();
+    const response = await fetch(
+      `https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${pokemonNameOrId}`
+    );
+    const data = await response.json();
+
+    // Set Pokemon info
+    pokemonName.textContent = `${data.name.toUpperCase()}`;
+    pokemonID.textContent = `#${data.id}`;
+    weight.textContent = `Weight: ${data.weight}`;
+  }
+};
