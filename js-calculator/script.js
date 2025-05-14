42c1b88f45fddce25a0351d51264347dccd0b889
jakemaloney <jakemal85@gmail.com>
2025-05-14 11:17:54 +1000

    updated the logic to be more modular

---
+++ js-calculator/script.js
@@ -0,0 +1,90 @@
+const buttons = document.querySelectorAll("button");
+
+let storedCalculation = []
+let total = ""
+const screenDisplay = document.querySelector("#display");
+
+
+function appendToDisplay(value){
+  if(screenDisplay.textContent === "0" || screenDisplay.textContent === "Error"){
+    screenDisplay.textContent = value;
+    storedCalculation = [value];
+  } else {
+    screenDisplay.textContent += value;
+    storedCalculation.push(value);
+  }
+  total = storedCalculation.join("")
+}
+
+function clearScreen(){
+  storedCalculation = [];
+  total = "";
+  screenDisplay.textContent = 0;
+}
+
+function evaluateExpression() {
+  try {
+    const expression = total.replace(/÷/g, "/").replace(/×/g, "*").replace(/−/g, "-")
+    const result = eval(expression)
+
+    screenDisplay.textContent = result
+    storedCalculation = [result];
+    total = result.toString();
+
+  } catch {
+    screenDisplay.textContent = "Error"
+    storedCalculation = []
+    total = ""
+  }
+}
+
+function convertToPercent(){
+  const currentNumber = parseFloat(storedCalculation) || 0;
+  const percent = currentNumber / 100;
+  screenDisplay.textContent = percent;
+  storedCalculation = [percent]
+  total = percent.toString()
+}
+
+function toggleSign(){
+  if (screenDisplay.textContent.startsWith("−")){
+    screenDisplay.textContent = screenDisplay.textContent.slice(1)
+  } else {
+    screenDisplay.textContent = "−" + screenDisplay.textContent
+  }
+  storedCalculation = [screenDisplay.textContent];
+  total = screenDisplay.textContent;
+
+}
+
+
+function calculate(button) {
+  const value = button.textContent;
+  console.log("Clicked:", value);
+
+  switch (value) {
+    case "AC":
+      clearScreen();
+      break;
+    case "=":
+      evaluateExpression();
+      break;
+    case "%":
+      convertToPercent();
+      break;
+    case "+/-":
+      toggleSign();
+      break;
+    default:
+      appendToDisplay(value);
+      break;
+  }
+}
+
+function initCalculator() {
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => calculate(button));
+  });
+}
+
+initCalculator();
